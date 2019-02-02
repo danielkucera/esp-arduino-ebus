bb82117d66115f8de22ef91b8b007ec8f4dd0007
Daniel Kucera <daniel.kucera@gmail.com>
2019-02-02 09:50:46 +0100

    working relay with ota

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include "config.h"
  
-const char* ssid = "yourNetworkName";
-const char* password =  "yourNetworkPass";
+const char* ssid = WIFI_SSID;
+const char* password =  WIFI_PASS;
  
 WiFiServer wifiServer(3333);
  
@@ -18,101 +19,36 @@ void setup() {
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    //Serial.println("Connecting..");
   }
- 
-  //Serial.print("Connected to WiFi. IP:");
-  //Serial.println(WiFi.localIP());
  
   wifiServer.begin();
 
   ArduinoOTA.begin();
 }
 
-int echo_read_byte(WiFiClient client){
-  int B;
-  int waited = 0;
-
-  // wait until data available
-  while (!client.available()){
-    if (waited++ > 30){
-      return -1;
-    }
-    delay(1);
-  }
-
-  B = client.read(); // read byte
-  client.write(B); // echo back
-  return B;
-}
-
-/* 
-  Reads and echoes data from client until full request is received.
-  Then waits for 0xaa on bus, sends the request and releases execution.
-*/
-void processRequest(WiFiClient client){
-  char request[255];
-  int i, B;
-  int len = 6;
-
-  // read full request
-  for (i=0; i < len; i++){
-    B = echo_read_byte(client);
-    if (B < 0) {
-      return;
-    }
-    request[i] = B;
-    if (i == 4) {
-      len = 6 + B;
-    }
-
-  }
-
-  int b1, b2;
-
-  // wait until 0xaa on bus
-  while (true){
-    b1 = Serial.read();
-    if ((b1 = -1) && (b2 = 0xaa)){
-      break;
-    }
-    b2 = b1;
-  }
-
-  Serial.write((uint8_t*)request,len);
-  Serial.readBytes(request, len);
-
-}
-
 int B;
+
+WiFiClient client;
 
 void loop() {
   ArduinoOTA.handle();
- 
-  WiFiClient client = wifiServer.available();
- 
-  if (client) {
+
+  if (wifiServer.hasClient()){
+    client = wifiServer.available();
     client.setNoDelay(true);
- 
-    while (client.connected()) {
- 
-      if (client.available()>0) {
-        // process client request here
-        processRequest(client);
-
-      } else {
-        // send data from serial to client
-        if (Serial.available()) {
-          B = Serial.read();
-          client.write(B);
-        }
-
-      }
-
-    }
- 
-    client.stop();
-    //Serial.println("Client disconnected");
- 
   }
+
+  if (client && client.connected()) {
+ 
+    if (client.available()>0) {
+      B = client.read();
+      Serial.write(B);
+
+    } else if (Serial.available()) {
+      B = Serial.read();
+      client.write(B);
+    }
+
+  }
+
 }
