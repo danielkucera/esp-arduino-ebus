219478a4dc2828283e8561d9ff13e071daec1e33
ysard <ysard@users.noreply.github.com>
2023-01-06 17:09:59 +0100

    Improve the processing of characters from TCP to UART Using a buffer to read & write multiple bytes at a time. Maybe we could reduce it's size, but now it's RXBUFFERSIZE
    Conflicts:
    	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,8 @@ WiFiClient serverClientsRO[MAX_SRV_CLIENTS];
 
 unsigned long last_comms;
 
+unsigned char txBuf[RXBUFFERSIZE];
+
 int random_ch(){
 #ifdef ESP32
   return 6;
@@ -188,9 +190,18 @@ void loop() {
 
   //check TCP clients for data
   for (int i = 0; i < MAX_SRV_CLIENTS; i++){
-    while (serverClients[i].available() && Serial.availableForWrite() > 0) {
-      // working char by char is not very efficient
-      Serial.write(serverClients[i].read());
+    size_t client_data_len = serverClients[i].available();
+    size_t authorized_len = (client_data_len > RXBUFFERSIZE) ? RXBUFFERSIZE : client_data_len;
+
+    while (authorized_len && Serial.availableForWrite() > 0) {
+      size_t ret = serverClients[i].readBytes(txBuf, authorized_len);
+      if (ret < authorized_len)
+        // Error
+        continue;
+      Serial.write(txBuf, authorized_len);
+
+      client_data_len = serverClients[i].available();
+      authorized_len = (client_data_len > RXBUFFERSIZE) ? RXBUFFERSIZE : client_data_len;
     }
   }
 
