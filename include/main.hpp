a4fd6f2b0993bf5936921a043b084fd1e6e68dac
Daniel Kucera <daniel.kucera@gmail.com>
2023-02-06 16:27:24 +0100

    split files

---
+++ include/main.hpp
@@ -0,0 +1,27 @@
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+
+#define MAX_SRV_CLIENTS 4
+#define RXBUFFERSIZE 1024
+#define STACK_PROTECTOR  512 // bytes
+#define HOSTNAME "esp-eBus"
+#define RESET_PIN 5
+#define RESET_MS 1000
+
+#ifndef TX_DISABLE_PIN
+#define TX_DISABLE_PIN 5
+#endif
+
+#ifdef ESP32
+// https://esp32.com/viewtopic.php?t=19788
+#define AVAILABLE_THRESHOLD 0
+#else
+#define AVAILABLE_THRESHOLD 1
+#endif
+
+bool handleNewClient(WiFiServer &server, WiFiClient clients[]);
+int pushClient(WiFiClient* client, uint8_t B);
+void handleClient(WiFiClient* client);
+
+int pushEnhClient(WiFiClient* client, uint8_t B);
+void handleEnhClient(WiFiClient* client);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include "main.hpp"
 
 #ifdef ESP32
   #include <esp_task_wdt.h>
@@ -10,29 +10,13 @@
   #include <ESP8266TrueRandom.h>
 #endif
 
-#define MAX_SRV_CLIENTS 4
-#define RXBUFFERSIZE 1024
-#define STACK_PROTECTOR  512 // bytes
-#define HOSTNAME "esp-eBus"
-#define RESET_PIN 5
-#define RESET_MS 1000
-
-#ifndef TX_DISABLE_PIN
-#define TX_DISABLE_PIN 5
-#endif
-
-#ifdef ESP32
-// https://esp32.com/viewtopic.php?t=19788
-#define AVAILABLE_THRESHOLD 0
-#else
-#define AVAILABLE_THRESHOLD 1
-#endif
-
 WiFiServer wifiServer(3333);
 WiFiServer wifiServerRO(3334);
+WiFiServer wifiServerEnh(3335);
 WiFiServer statusServer(5555);
 WiFiClient serverClients[MAX_SRV_CLIENTS];
 WiFiClient serverClientsRO[MAX_SRV_CLIENTS];
+WiFiClient enhClients[MAX_SRV_CLIENTS];
 
 unsigned long last_comms;
 
@@ -149,33 +133,6 @@ bool handleStatusServerRequests() {
 }
 
 
-bool handleNewClient(WiFiServer &server, WiFiClient clients[]) {
-  if (!server.hasClient())
-    return false;
-
-  // Find free/disconnected slot
-  int i;
-  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-    if (!clients[i]) { // equivalent to !serverClients[i].connected()
-      clients[i] = server.available();
-      clients[i].setNoDelay(true);
-      break;
-    }
-  }
-
-  // No free/disconnected slot so reject
-  if (i == MAX_SRV_CLIENTS) {
-    server.available().println("busy");
-    // hints: server.available() is a WiFiClient with short-term scope
-    // when out of scope, a WiFiClient will
-    // - flush() - all data will be sent
-    // - stop() - automatically too
-  }
-
-  return true;
-}
-
-
 void loop() {
   ArduinoOTA.handle();
 
@@ -197,42 +154,35 @@ void loop() {
   handleStatusServerRequests();
 
   // Check if there are any new clients on the eBUS servers
-  if (handleNewClient(wifiServer, serverClients))
+  if (handleNewClient(wifiServer, serverClients)){
     enableTX();
+  }
   handleNewClient(wifiServerRO, serverClientsRO);
+  handleNewClient(wifiServerEnh, enhClients);
 
-  //check TCP clients for data
+  //check clients for data
   for (int i = 0; i < MAX_SRV_CLIENTS; i++){
-    while (serverClients[i].available() && Serial.availableForWrite() > 0) {
-      // working char by char is not very efficient
-      Serial.write(serverClients[i].read());
-    }
+    handleClient(&serverClients[i]);
+    handleEnhClient(&enhClients[i]);
   }
 
   //check UART for data
-  size_t len = Serial.available();
-  if (len) {
+  if (size_t len = Serial.available()) {
     byte B = Serial.read();
-    // push UART data to all connected telnet clients
+
+    // push data to clients
     for (int i = 0; i < MAX_SRV_CLIENTS; i++){
-      // if client.availableForWrite() was 0 (congested)
-      // and increased since then,
-      // ensure write space is sufficient:
-      if (serverClients[i].availableForWrite() >= AVAILABLE_THRESHOLD) {
-        serverClients[i].write(B);
+      if (pushClient(&serverClients[i], B)){
+        last_comms = millis();
+      }
+      if (pushClient(&serverClientsRO[i], B)){
+        last_comms = millis();
+      }
+      if (pushEnhClient(&enhClients[i], B)){
         last_comms = millis();
       }
     }
-    // push UART data to all connected readonly clients
-    for (int i = 0; i < MAX_SRV_CLIENTS; i++){
-      // if client.availableForWrite() was 0 (congested)
-      // and increased since then,
-      // ensure write space is sufficient:
-      if (serverClientsRO[i].availableForWrite() >= AVAILABLE_THRESHOLD) {
-        serverClientsRO[i].write(B);
-        last_comms = millis();
-      }
-    }
+
   }
 
 }
