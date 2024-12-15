03f624af5a295527bb33b594d949aefbc98d84bf
Roland Jax <roland.jax@liwest.at>
2024-12-15 23:04:46 +0100

    formatting - Google style (#90)
    * formatting
    
    * some adjustments

--- include/arbitration.hpp
+++ include/arbitration.hpp
@@ -3,52 +3,53 @@
 
 #include "busstate.hpp"
 
-// Implements the arbitration algorithm. Uses the state of the bus to decide what to do.
-// Typical usage:
+// Implements the arbitration algorithm. Uses the state of the bus to decide
+// what to do. Typical usage:
 // - try to start the arbitration with "start" method
 // - pass each received value on the bus to the "data" method
 //   which will then tell you what the state of the arbitration is
-class Arbitration
-{
-    public:
-        enum state {none,        // no arbitration ongoing
-                    arbitrating, // arbitration ongoing
-                    won1,        // won
-                    won2,        // won
-                    lost1,       // lost
-                    lost2,       // lost
-                    error,       // error
-                    restart1,    // restart the arbitration
-                    restart2,    // restart the arbitration
-                    };
+class Arbitration {
+ public:
+  enum state {
+    none,         // no arbitration ongoing
+    arbitrating,  // arbitration ongoing
+    won1,         // won
+    won2,         // won
+    lost1,        // lost
+    lost2,        // lost
+    error,        // error
+    restart1,     // restart the arbitration
+    restart2,     // restart the arbitration
+  };
 
-        Arbitration()
-        : _arbitrating(false)
-        , _participateSecond(false)
-        , _arbitrationAddress(0)
-        , _restartCount(0)
-        {}
-    // Try to start arbitration for the specified master.
-    // Return values:
-    // - started    : arbitration started. Make sure to pass all bus data to this object through the "data" method
-    // - not_started : arbitration not started. Possible reasons:
-    //                + the bus is not in a state that allows to start arbitration
-    //                + another arbitration is already ongoing
-    //                + the master address is SYN
-    // - late       : arbitration not started because the start is too late compared to the SYN symbol received
-    enum result {started, not_started, late};
-    result start(BusState& busstate, uint8_t master, unsigned long startBitTime);
+  Arbitration()
+      : _arbitrating(false),
+        _participateSecond(false),
+        _arbitrationAddress(0),
+        _restartCount(0) {}
+  // Try to start arbitration for the specified master.
+  // Return values:
+  // - started     : arbitration started. Make sure to pass all bus data to this
+  //                object through the "data" method
+  // - not_started : arbitration not started. Possible reasons:
+  //                + the bus is not in a state that allows to start arbitration
+  //                + another arbitration is already ongoing
+  //                + the master address is SYN
+  // - late        : arbitration not started because the start is too late
+  //                 compared to the SYN symbol received
+  enum result { started, not_started, late };
+  result start(BusState& busstate, uint8_t master, unsigned long startBitTime);
 
-    // A symbol was received on the bus, what does this do to the arbitration state?
-    // Return values:
-    // - see description of state enum value
-    Arbitration::state data   (BusState& busstate, uint8_t symbol, unsigned long startBitTime);
+  // A symbol was received on the bus, what does this do to the arbitration
+  // state? Return values: see description of state enum value
+  Arbitration::state data(BusState& busstate, uint8_t symbol,
+                          unsigned long startBitTime);
 
-    private:
-        bool    _arbitrating;
-        bool    _participateSecond;
-        uint8_t _arbitrationAddress;
-        int     _restartCount;
+ private:
+  bool _arbitrating;
+  bool _participateSecond;
+  uint8_t _arbitrationAddress;
+  int _restartCount;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,29 @@
+#include "main.hpp"
+
 #include <ArduinoOTA.h>
 #include <IotWebConf.h>
 #include <IotWebConfUsing.h>
 #include <Preferences.h>
-#include "main.hpp"
-#include "enhanced.hpp"
+
 #include "bus.hpp"
+#include "enhanced.hpp"
 
 Preferences preferences;
 
 #ifdef ESP32
-  #include <esp_task_wdt.h>
-  #include <ESPmDNS.h>
-  #include "esp32c3/rom/rtc.h"
-  #include <IotWebConfESP32HTTPUpdateServer.h>
+#include <ESPmDNS.h>
+#include <IotWebConfESP32HTTPUpdateServer.h>
+#include <esp_task_wdt.h>
 
-  HTTPUpdateServer httpUpdater;
+#include "esp32c3/rom/rtc.h"
+
+HTTPUpdateServer httpUpdater;
 #else
-  #include <ESP8266mDNS.h>
-  #include <ESP8266TrueRandom.h>
-  #include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266TrueRandom.h>
+#include <ESP8266mDNS.h>
 
-  ESP8266HTTPUpdateServer httpUpdater;
+ESP8266HTTPUpdateServer httpUpdater;
 #endif
 
 #define ALPHA 0.3
@@ -56,13 +59,22 @@ char gatewayValue[STRING_LEN];
 char netmaskValue[STRING_LEN];
 
 IotWebConf iotWebConf(HOSTNAME, &dnsServer, &configServer, "", CONFIG_VERSION);
-IotWebConfNumberParameter pwmParam = IotWebConfNumberParameter("PWM value", "pwm_value", pwm_value, NUMBER_LEN, "130", "1..255", "min='1' max='255' step='1'");
+IotWebConfNumberParameter pwmParam =
+    IotWebConfNumberParameter("PWM value", "pwm_value", pwm_value, NUMBER_LEN,
+                              "130", "1..255", "min='1' max='255' step='1'");
 
-IotWebConfParameterGroup connGroup = IotWebConfParameterGroup("conn", "Connection parameters");
-IotWebConfCheckboxParameter staticIPParam = IotWebConfCheckboxParameter("Enable Static IP", "staticIPParam", staticIPValue, STRING_LEN);
-IotWebConfTextParameter ipAddressParam = IotWebConfTextParameter("IP address", "ipAddress", ipAddressValue, STRING_LEN, "", DEFAULT_STATIC_IP);
-IotWebConfTextParameter gatewayParam = IotWebConfTextParameter("Gateway", "gateway", gatewayValue, STRING_LEN, "", DEFAULT_GATEWAY);
-IotWebConfTextParameter netmaskParam = IotWebConfTextParameter("Subnet mask", "netmask", netmaskValue, STRING_LEN, DEFAULT_NETMASK, DEFAULT_NETMASK);
+IotWebConfParameterGroup connGroup =
+    IotWebConfParameterGroup("conn", "Connection parameters");
+IotWebConfCheckboxParameter staticIPParam = IotWebConfCheckboxParameter(
+    "Enable Static IP", "staticIPParam", staticIPValue, STRING_LEN);
+IotWebConfTextParameter ipAddressParam =
+    IotWebConfTextParameter("IP address", "ipAddress", ipAddressValue,
+                            STRING_LEN, "", DEFAULT_STATIC_IP);
+IotWebConfTextParameter gatewayParam = IotWebConfTextParameter(
+    "Gateway", "gateway", gatewayValue, STRING_LEN, "", DEFAULT_GATEWAY);
+IotWebConfTextParameter netmaskParam =
+    IotWebConfTextParameter("Subnet mask", "netmask", netmaskValue, STRING_LEN,
+                            DEFAULT_NETMASK, DEFAULT_NETMASK);
 
 IPAddress ipAddress;
 IPAddress gateway;
@@ -84,9 +96,9 @@ unsigned long maxLoopDuration = 0;
 unsigned long lastConnectTime = 0;
 int reconnectCount = 0;
 
-int random_ch(){
+int random_ch() {
 #ifdef ESP32
-  return esp_random() % 13 + 1 ;
+  return esp_random() % 13 + 1;
 #elif defined(ESP8266)
   return ESP8266TrueRandom.random(1, 13);
 #endif
@@ -123,20 +135,20 @@ inline void enableTX() {
 #endif
 }
 
-void set_pwm(uint8_t value){
+void set_pwm(uint8_t value) {
 #ifdef PWM_PIN
   ledcWrite(PWM_CHANNEL, value);
 #endif
 }
 
-uint32_t get_pwm(){
+uint32_t get_pwm() {
 #ifdef PWM_PIN
   return ledcRead(PWM_CHANNEL);
 #endif
   return 0;
 }
 
-void reset(){
+void reset() {
   disableTX();
   ESP.restart();
 }
@@ -150,8 +162,8 @@ void check_reset() {
   // check if RESET_PIN being hold low and reset
   pinMode(RESET_PIN, INPUT_PULLUP);
   unsigned long resetStart = millis();
-  while(digitalRead(RESET_PIN) == 0){
-    if (millis() > resetStart + RESET_MS){
+  while (digitalRead(RESET_PIN) == 0) {
+    if (millis() > resetStart + RESET_MS) {
       reset_config();
     }
   }
@@ -171,35 +183,35 @@ void loop_duration() {
   }
 }
 
-void data_process(){
+void data_process() {
   loop_duration();
 
-  //check clients for data
-  for (int i = 0; i < MAX_SRV_CLIENTS; i++){
+  // check clients for data
+  for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
     handleClient(&serverClients[i]);
     handleEnhClient(&enhClients[i]);
   }
 
-  //check queue for data
+  // check queue for data
   BusType::data d;
   if (Bus.read(d)) {
-    for (int i = 0; i < MAX_SRV_CLIENTS; i++){
+    for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
       if (d._enhanced) {
         if (d._client == &enhClients[i]) {
           if (pushEnhClient(&enhClients[i], d._c, d._d, true)) {
             last_comms = millis();
           }
         }
-      }
-      else {
-        if (pushClient(&serverClients[i], d._d)){
+      } else {
+        if (pushClient(&serverClients[i], d._d)) {
           last_comms = millis();
         }
-        if (pushClient(&serverClientsRO[i], d._d)){
+        if (pushClient(&serverClientsRO[i], d._d)) {
           last_comms = millis();
         }
         if (d._client != &enhClients[i]) {
-          if (pushEnhClient(&enhClients[i], d._c, d._d, d._logtoclient == &enhClients[i])){
+          if (pushEnhClient(&enhClients[i], d._c, d._d,
+                            d._logtoclient == &enhClients[i])) {
             last_comms = millis();
           }
         }
@@ -208,8 +220,8 @@ void data_process(){
   }
 }
 
-void data_loop(void * pvParameters){
-  while(1){
+void data_loop(void* pvParameters) {
+  while (1) {
     data_process();
   }
 }
@@ -217,7 +229,7 @@ void data_loop(void * pvParameters){
 bool formValidator(iotwebconf::WebRequestWrapper* webRequestWrapper) {
   bool valid = true;
 
-  if (webRequestWrapper->arg(staticIPParam.getId()).equals("selected")) {  
+  if (webRequestWrapper->arg(staticIPParam.getId()).equals("selected")) {
     if (!ipAddress.fromString(webRequestWrapper->arg(ipAddressParam.getId()))) {
       ipAddressParam.errorMessage = "Please provide a valid IP address!";
       valid = false;
@@ -230,14 +242,12 @@ bool formValidator(iotwebconf::WebRequestWrapper* webRequestWrapper) {
       gatewayParam.errorMessage = "Please provide a valid gateway address!";
       valid = false;
     }
-  } 
+  }
 
   return valid;
 }
 
-void saveParamsCallback () {
-  set_pwm(atoi(pwm_value));
-}
+void saveParamsCallback() { set_pwm(atoi(pwm_value)); }
 
 void connectWifi(const char* ssid, const char* password) {
   if (staticIPParam.isChecked()) {
@@ -245,11 +255,10 @@ void connectWifi(const char* ssid, const char* password) {
     valid = valid && ipAddress.fromString(String(ipAddressValue));
     valid = valid && netmask.fromString(String(netmaskValue));
     valid = valid && gateway.fromString(String(gatewayValue));
-    
-    if (valid)  
-      WiFi.config(ipAddress, gateway, netmask);
+
+    if (valid) WiFi.config(ipAddress, gateway, netmask);
   }
- 
+
   WiFi.begin(ssid, password);
 }
 
@@ -258,13 +267,15 @@ void wifiConnected() {
   reconnectCount++;
 }
 
-char* status_string(){
+char* status_string() {
   static char status[1024];
 
   int pos = 0;
 
-  pos += sprintf(status + pos, "async mode: %s\n", USE_ASYNCHRONOUS ? "true" : "false");
-  pos += sprintf(status + pos, "software serial mode: %s\n", USE_SOFTWARE_SERIAL ? "true" : "false");
+  pos += sprintf(status + pos, "async mode: %s\n",
+                 USE_ASYNCHRONOUS ? "true" : "false");
+  pos += sprintf(status + pos, "software serial mode: %s\n",
+                 USE_SOFTWARE_SERIAL ? "true" : "false");
   pos += sprintf(status + pos, "uptime: %ld ms\n", millis());
   pos += sprintf(status + pos, "last_connect_time: %ld ms\n", lastConnectTime);
   pos += sprintf(status + pos, "reconnect_count: %d \n", reconnectCount);
@@ -272,9 +283,11 @@ char* status_string(){
   pos += sprintf(status + pos, "free_heap: %d B\n", ESP.getFreeHeap());
   pos += sprintf(status + pos, "reset_code: %d\n", last_reset_code);
   pos += sprintf(status + pos, "loop_duration: %ld us\r\n", loopDuration);
-  pos += sprintf(status + pos, "max_loop_duration: %ld us\r\n", maxLoopDuration);
+  pos +=
+      sprintf(status + pos, "max_loop_duration: %ld us\r\n", maxLoopDuration);
   pos += sprintf(status + pos, "version: %s\r\n", AUTO_VERSION);
-  pos += sprintf(status + pos, "nbr arbitrations: %i\r\n", (int)Bus._nbrArbitrations);
+  pos += sprintf(status + pos, "nbr arbitrations: %i\r\n",
+                 (int)Bus._nbrArbitrations);
   pos += sprintf(status + pos, "nbr restarts1: %i\r\n", (int)Bus._nbrRestarts1);
   pos += sprintf(status + pos, "nbr restarts2: %i\r\n", (int)Bus._nbrRestarts2);
   pos += sprintf(status + pos, "nbr lost1: %i\r\n", (int)Bus._nbrLost1);
@@ -288,30 +301,28 @@ char* status_string(){
   return status;
 }
 
-void handleStatus()
-{
-  configServer.send(200, "text/plain", status_string());
-}
+void handleStatus() { configServer.send(200, "text/plain", status_string()); }
 
-
-void handleRoot()
-{
+void handleRoot() {
   // -- Let IotWebConf test and handle captive portal requests.
-  if (iotWebConf.handleCaptivePortal())
-  {
+  if (iotWebConf.handleCaptivePortal()) {
     // -- Captive portal request were already served.
     return;
   }
   String s = "<html><head><title>esp-eBus adapter</title>";
-  s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, "
+       "user-scalable=no\"/>";
   s += "</head><body>";
   s += "<a href='/status'>Adapter status</a><br>";
-  s += "<a href='/config'>Configuration</a> - user: admin password: your configured AP mode password or default: ";
+  s += "<a href='/config'>Configuration</a> - user: admin password: your "
+       "configured AP mode password or default: ";
   s += DEFAULT_APMODE_PASS;
   s += "<br>";
   s += "<a href='/firmware'>Firmware update</a><br>";
   s += "<br>";
-  s += "For more info see project page: <a href='https://github.com/danielkucera/esp-arduino-ebus'>https://github.com/danielkucera/esp-arduino-ebus</a>";
+  s += "For more info see project page: <a "
+       "href='https://github.com/danielkucera/esp-arduino-ebus'>https://"
+       "github.com/danielkucera/esp-arduino-ebus</a>";
   s += "</body></html>";
 
   configServer.send(200, "text/html", s);
@@ -325,7 +336,7 @@ void setup() {
 #ifdef ESP32
   last_reset_code = rtc_get_reset_reason(0);
 #elif defined(ESP8266)
-  last_reset_code = (int) ESP.getResetInfoPtr();
+  last_reset_code = (int)ESP.getResetInfoPtr();
 #endif
   Bus.begin();
 
@@ -339,22 +350,26 @@ void setup() {
   ledcAttachPin(PWM_PIN, PWM_CHANNEL);
 #endif
 
-  if (preferences.getBool("firstboot", true)){
+  if (preferences.getBool("firstboot", true)) {
     preferences.putBool("firstboot", false);
-    
+
     iotWebConf.init();
-    strncpy(iotWebConf.getApPasswordParameter()->valueBuffer, DEFAULT_APMODE_PASS, IOTWEBCONF_WORD_LEN);
-    strncpy(iotWebConf.getWifiSsidParameter()->valueBuffer, "ebus-test", IOTWEBCONF_WORD_LEN);
-    strncpy(iotWebConf.getWifiPasswordParameter()->valueBuffer, "lectronz", IOTWEBCONF_WORD_LEN);
+    strncpy(iotWebConf.getApPasswordParameter()->valueBuffer,
+            DEFAULT_APMODE_PASS, IOTWEBCONF_WORD_LEN);
+    strncpy(iotWebConf.getWifiSsidParameter()->valueBuffer, "ebus-test",
+            IOTWEBCONF_WORD_LEN);
+    strncpy(iotWebConf.getWifiPasswordParameter()->valueBuffer, "lectronz",
+            IOTWEBCONF_WORD_LEN);
     iotWebConf.saveConfig();
 
-    WiFi.channel(random_ch()); // doesn't work, https://github.com/prampec/IotWebConf/issues/286
-  } 
-  else {
+    WiFi.channel(
+        random_ch());  // doesn't work,
+                       // https://github.com/prampec/IotWebConf/issues/286
+  } else {
     iotWebConf.skipApStartup();
   }
 
-  connGroup.addItem(&staticIPParam);  
+  connGroup.addItem(&staticIPParam);
   connGroup.addItem(&ipAddressParam);
   connGroup.addItem(&gatewayParam);
   connGroup.addItem(&netmaskParam);
@@ -376,20 +391,24 @@ void setup() {
   iotWebConf.init();
 
   // -- Set up required URL handlers on the web server.
-  configServer.on("/", []{ handleRoot(); });
-  configServer.on("/config", []{ iotWebConf.handleConfig(); });
-  configServer.on("/param", []{ iotWebConf.handleConfig(); });
-  configServer.on("/status", []{ handleStatus(); });
-  configServer.onNotFound([](){ iotWebConf.handleNotFound(); });
+  configServer.on("/", [] { handleRoot(); });
+  configServer.on("/config", [] { iotWebConf.handleConfig(); });
+  configServer.on("/param", [] { iotWebConf.handleConfig(); });
+  configServer.on("/status", [] { handleStatus(); });
+  configServer.onNotFound([]() { iotWebConf.handleNotFound(); });
 
   iotWebConf.setupUpdateServer(
-    [](const char* updatePath) { httpUpdater.setup(&configServer, updatePath); },
-    [](const char* userName, char* password) { httpUpdater.updateCredentials(userName, password); });
+      [](const char* updatePath) {
+        httpUpdater.setup(&configServer, updatePath);
+      },
+      [](const char* userName, char* password) {
+        httpUpdater.updateCredentials(userName, password);
+      });
 
   set_pwm(atoi(pwm_value));
 
-  while (iotWebConf.getState() != iotwebconf::NetworkState::OnLine){
-      iotWebConf.doLoop();
+  while (iotWebConf.getState() != iotwebconf::NetworkState::OnLine) {
+    iotWebConf.doLoop();
   }
 
   wifiServer.begin();
@@ -411,8 +430,7 @@ void setup() {
 }
 
 bool handleStatusServerRequests() {
-  if (!statusServer.hasClient())
-    return false;
+  if (!statusServer.hasClient()) return false;
 
   WiFiClient client = statusServer.accept();
 
@@ -439,7 +457,7 @@ void loop() {
   iotWebConf.doLoop();
 #endif
 
-  if (millis() > last_comms + 200*1000 ) {
+  if (millis() > last_comms + 200 * 1000) {
     reset();
   }
 
@@ -452,13 +470,12 @@ void loop() {
   }
 
   // Check if there are any new clients on the eBUS servers
-  if (handleNewClient(wifiServer, serverClients)){
+  if (handleNewClient(wifiServer, serverClients)) {
     enableTX();
   }
-  if (handleNewClient(wifiServerEnh, enhClients)){
+  if (handleNewClient(wifiServerEnh, enhClients)) {
     enableTX();
   }
 
   handleNewClient(wifiServerRO, serverClientsRO);
-
 }
