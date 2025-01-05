deed81dd6f515c6b0b96cf70abbf05fb57b77d9c
Roland Jax <roland.jax@liwest.at>
2025-01-05 07:42:06 +0100

    #pragma once instead of #ifndef #define #endif

--- include/enhanced.hpp
+++ include/enhanced.hpp
@@ -1,9 +1,6 @@
-#ifndef INCLUDE_ENHANCED_HPP_
-#define INCLUDE_ENHANCED_HPP_
+#pragma once
 
 #include <WiFiClient.h>
 
 int pushEnhClient(WiFiClient* client, uint8_t c, uint8_t d, bool log);
 void handleEnhClient(WiFiClient* client);
-
-#endif  // INCLUDE_ENHANCED_HPP_
