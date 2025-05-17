ff2577ec05cb66acaa10ac4ab06d22d2d726856e
ajvdw <ajvandewerken@gmail.com>
2025-05-17 15:07:53 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -480,9 +480,102 @@ char* status_string() {
 void handleStatus() { configServer.send(200, "text/plain", status_string()); }
 
 #ifdef EBUS_INTERNAL
-void handleCommands() {
+void handleCommandsList() {
   configServer.send(200, "application/json;charset=utf-8",
                     store.getCommandsJson().c_str());
+}
+
+void handleCommandsUpload() {
+   configServer.send(200, "text/html", F(R"(<html>
+<head><title>esp-eBus adapter</title>
+<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'>
+</head><body>
+</body></html>
+<h3>Upload Commands</h3>
+<input type='file' id='file-upload'>
+<script type='text/javascript'>
+window.onload = function(event) {
+  document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
+}
+function handleFileSelect(event) {
+  var fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    try {
+      var commands=JSON.parse(event.target.result);
+      var headers = new Headers();
+      headers.append('Accept', 'text/plain');
+      headers.append('Content-Type', 'text/plain');
+      fetch('/commands/insert', { method: 'POST', headers: headers, body: JSON.stringify(commands) } )
+      .then( response => {  alert(response.ok?"Succesfully loaded":"Something went wrong"); } );
+    } catch (error) {
+      alert('Invalid JSON Config file');
+    }
+  }
+  var file = event.target.files[0];
+  fileReader.readAsText(file);
+}
+</script>
+  
+)") );
+}
+
+void handleCommandsDownload(){
+  String s = "{ \"id\": \"insert\",\n   \"commands\": ";
+    s += store.getCommandsJson().c_str();
+    s += "\n}";
+  configServer.send(200, "application/json", s );
+}
+
+void handleCommandsLoad() {
+  int64_t bytes = store.loadCommands();
+  if (bytes > 0)
+    configServer.send(200, "text/html","Successfully loaded");
+   else if (bytes < 0)
+    configServer.send(200, "text/html","Loading failed");
+  else
+    configServer.send(200, "text/html","No data loaded");
+}
+
+void handleCommandsSave() {
+  int64_t bytes = store.saveCommands();
+  if (bytes > 0)
+    configServer.send(200, "text/html","Successfully saved");
+   else if (bytes < 0)
+    configServer.send(200, "text/html","Saving failed");
+  else
+    configServer.send(200, "text/html","No data saved");
+}
+
+void handleCommandsWipe() {
+  int64_t bytes = store.wipeCommands();
+  if (bytes > 0)
+    configServer.send(200, "text/html","Successfully wiped");
+   else if (bytes < 0)
+    configServer.send(200, "text/html","Wiping failed");
+  else
+    configServer.send(200, "text/html","No data wiped");
+}
+
+void handleCommandsInsert() {
+  JsonDocument doc;
+  String body = configServer.arg("plain"); 
+ 
+  DeserializationError error = deserializeJson(doc, body);
+
+  if (error)
+    configServer.send(403, "text/html", "INVALID JSON");
+  else
+  {
+    JsonArray array = doc["commands"].as<JsonArray>();
+    if (array != nullptr) 
+    {
+      for (JsonVariant variant : array)
+          store.insertCommand( store.createCommand(variant) );
+      configServer.send(200, "text/html", "OK");
+    }
+    else
+      configServer.send(403, "text/html", "NO COMMANDS");
+  }
 }
 
 void handleValues() {
@@ -571,8 +664,14 @@ void handleRoot() {
   s += "<a href='/status'>Adapter status</a><br>";
 
 #ifdef EBUS_INTERNAL
-  s += "<a href='/commands'>Commands</a><br>";
+  s += "<a href='/commands/list'>List commands</a><br>";
+  s += "<a href='/commands/upload'>Upload commands</a><br>";
+  s += "<a href='/commands/download'>Download commands</a><br>";
+  s += "<a href='/commands/load'>Load commands</a><br>";
+  s += "<a href='/commands/save'>Save commands</a><br>";
+  s += "<a href='/commands/wipe'>Wipe commands</a><br>";
   s += "<a href='/values'>Values</a><br>";
+  s += "<a href='/restart'>Restart</a><br>";
 #endif
 
   s += "<a href='/config'>Configuration</a> - user: admin password: your "
@@ -683,11 +782,16 @@ void setup() {
   configServer.on("/status", [] { handleStatus(); });
 
 #ifdef EBUS_INTERNAL
-  configServer.on("/commands", [] { handleCommands(); });
+  configServer.on("/commands/list", [] { handleCommandsList(); });
+  configServer.on("/commands/download", [] { handleCommandsDownload(); });
+  configServer.on("/commands/upload", [] { handleCommandsUpload(); });
+  configServer.on("/commands/insert", [] { handleCommandsInsert(); });
+  configServer.on("/commands/load", [] { handleCommandsLoad(); });
+  configServer.on("/commands/save", [] { handleCommandsSave(); });
+  configServer.on("/commands/wipe", [] { handleCommandsWipe(); });
   configServer.on("/values", [] { handleValues(); });
 #endif
-
-  configServer.on("/config", [] { iotWebConf.handleConfig(); });
+  configServer.on("/restart", [] { restart(); });
   configServer.onNotFound([]() { iotWebConf.handleNotFound(); });
 
   iotWebConf.setupUpdateServer(
