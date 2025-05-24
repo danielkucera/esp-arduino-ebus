ae99f07362c8ccec921c525f8e09f9b5a79eb11b
Arie Johan van de Werken <ajvandewerken@gmail.com>
2025-05-24 07:28:24 +0200

    Applied Google style guide formatting and fixed /config (#132)
    * Update main.cpp
    
    * Moved http stuff to http.ccp/hpp
    
    * Webserver support 8266 added
    
    * Update http.hpp
    
    * Update main.cpp
    
    * minor cleanup
    
    * make more room for binary
    
    * removed unnecessary includes
    
    * Update http.cpp
    
    * Update http.cpp
    
    * Update http.cpp
    
    * Update http.cpp

--- src/http.cpp
+++ src/http.cpp
@@ -1,17 +1,10 @@
-#ifdef ESP32
-#include <WebServer.h>
-#else
-#include <ESP8266WebServer.h>
-#endif
+#include "http.hpp"
 
 #include "main.hpp"
-#include "http.hpp"
 
 WebServer configServer(80);
 
-void handleStatus() { 
-  configServer.send(200, "text/plain", status_string()); 
-}
+void handleStatus() { configServer.send(200, "text/plain", status_string()); }
 
 #ifdef EBUS_INTERNAL
 void handleCommandsList() {
@@ -20,95 +13,92 @@ void handleCommandsList() {
 }
 
 void handleCommandsUpload() {
-   configServer.send(200, "text/html", F(R"(<html>
-<head><title>esp-eBus adapter</title>
-<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'>
-</head><body>
-</body></html>
-<h3>Upload Commands</h3>
-<input type='file' id='file-upload'>
-<script type='text/javascript'>
-window.onload = function(event) {
-  document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
-}
-function handleFileSelect(event) {
-  var fileReader = new FileReader();
-  fileReader.onload = function(event) {
-    try {
-      var commands=JSON.parse(event.target.result);
-      var headers = new Headers();
-      headers.append('Accept', 'text/plain');
-      headers.append('Content-Type', 'text/plain');
-      fetch('/commands/insert', { method: 'POST', headers: headers, body: JSON.stringify(commands) } )
-      .then( response => {  alert(response.ok?"Succesfully loaded":"Something went wrong"); } );
-    } catch (error) {
-      alert('Invalid JSON Config file');
-    }
+  configServer.send(200, "text/html", F(R"(<html>
+  <head><title>esp-eBus adapter</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'>
+  </head><body>
+  <h3>Upload Commands</h3>
+  <input type='file' id='file-upload'>
+  <script type='text/javascript'>
+  window.onload = function(event) {
+    document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
   }
-  var file = event.target.files[0];
-  fileReader.readAsText(file);
-}
-</script>
-  
-)") );
+  function handleFileSelect(event) {
+    var fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      try {
+        var commands=JSON.parse(event.target.result);
+        var headers = new Headers();
+        headers.append('Accept', 'text/plain');
+        headers.append('Content-Type', 'text/plain');
+        fetch('/commands/insert', { method: 'POST', headers: headers, body: JSON.stringify(commands) } )
+        .then( response => {  alert(response.ok?"Succesfully loaded":"Something went wrong"); } );
+      } catch (error) { 
+        alert('Invalid JSON Config file'); 
+      }
+    }
+    var file = event.target.files[0];
+    fileReader.readAsText(file);
+  }
+  </script>
+  </body></html>
+  )"));
 }
 
-void handleCommandsDownload(){
+void handleCommandsDownload() {
   String s = "{ \"id\": \"insert\",\n   \"commands\": ";
-    s += store.getCommandsJson().c_str();
-    s += "\n}";
-  configServer.send(200, "application/json", s );
+  s += store.getCommandsJson().c_str();
+  s += "\n}";
+  configServer.send(200, "application/json", s);
 }
 
 void handleCommandsLoad() {
   int64_t bytes = store.loadCommands();
   if (bytes > 0)
-    configServer.send(200, "text/html","Successfully loaded");
-   else if (bytes < 0)
-    configServer.send(200, "text/html","Loading failed");
+    configServer.send(200, "text/html", "Successfully loaded");
+  else if (bytes < 0)
+    configServer.send(200, "text/html", "Loading failed");
   else
-    configServer.send(200, "text/html","No data loaded");
+    configServer.send(200, "text/html", "No data loaded");
 }
 
 void handleCommandsSave() {
   int64_t bytes = store.saveCommands();
   if (bytes > 0)
-    configServer.send(200, "text/html","Successfully saved");
-   else if (bytes < 0)
-    configServer.send(200, "text/html","Saving failed");
+    configServer.send(200, "text/html", "Successfully saved");
+  else if (bytes < 0)
+    configServer.send(200, "text/html", "Saving failed");
   else
-    configServer.send(200, "text/html","No data saved");
+    configServer.send(200, "text/html", "No data saved");
 }
 
 void handleCommandsWipe() {
   int64_t bytes = store.wipeCommands();
   if (bytes > 0)
-    configServer.send(200, "text/html","Successfully wiped");
-   else if (bytes < 0)
-    configServer.send(200, "text/html","Wiping failed");
+    configServer.send(200, "text/html", "Successfully wiped");
+  else if (bytes < 0)
+    configServer.send(200, "text/html", "Wiping failed");
   else
-    configServer.send(200, "text/html","No data wiped");
+    configServer.send(200, "text/html", "No data wiped");
 }
 
 void handleCommandsInsert() {
   JsonDocument doc;
-  String body = configServer.arg("plain"); 
- 
+  String body = configServer.arg("plain");
+
   DeserializationError error = deserializeJson(doc, body);
 
-  if (error)
+  if (error) {
     configServer.send(403, "text/html", "INVALID JSON");
-  else
-  {
+  } else {
     JsonArray array = doc["commands"].as<JsonArray>();
-    if (array != nullptr) 
-    {
+    if (array != nullptr) {
       for (JsonVariant variant : array)
-          store.insertCommand( store.createCommand(variant) );
+        store.insertCommand(store.createCommand(variant));
       configServer.send(200, "text/html", "OK");
-    }
-    else
+    } else {
       configServer.send(403, "text/html", "NO COMMANDS");
+    }
   }
 }
 
@@ -117,6 +107,7 @@ void handleValues() {
                     store.getValuesJson().c_str());
 }
 #endif
+
 void handleRoot() {
   // -- Let IotWebConf test and handle captive portal requests.
   if (iotWebConf.handleCaptivePortal()) {
@@ -126,20 +117,26 @@ void handleRoot() {
   String s = "<html><head><title>esp-eBus adapter</title>";
   s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, "
        "user-scalable=no\"/>";
+  s += "  <script type='text/javascript'>";
+  s += "     function confirmAction(action){ return confirm(\"Are you sure you "
+       "want to \" + action + \"?\"); }\n";
+  s += "  </script>\n";
   s += "</head><body>";
   s += "<a href='/status'>Adapter status</a><br>";
-
 #ifdef EBUS_INTERNAL
   s += "<a href='/commands/list'>List commands</a><br>";
   s += "<a href='/commands/upload'>Upload commands</a><br>";
   s += "<a href='/commands/download'>Download commands</a><br>";
-  s += "<a href='/commands/load'>Load commands</a><br>";
-  s += "<a href='/commands/save'>Save commands</a><br>";
-  s += "<a href='/commands/wipe'>Wipe commands</a><br>";
+  s += "<a href='/commands/load' onclick=\"return "
+       "confirmAction('load commands');\">Load commands</a><br>";
+  s += "<a href='/commands/save' onclick=\"return "
+       "confirmAction('save commands');\">Save commands</a><br>";
+  s += "<a href='/commands/wipe' onclick=\"return "
+       "confirmAction('wipe commands');\">Wipe commands</a><br>";
   s += "<a href='/values'>Values</a><br>";
-  s += "<a href='/restart'>Restart</a><br>";
 #endif
-
+  s += "<a href='/restart' onclick=\"return "
+       "confirmAction('restart');\">Restart</a><br>";
   s += "<a href='/config'>Configuration</a> - user: admin password: your "
        "configured AP mode password or default password";
   s += "<br>";
@@ -153,16 +150,10 @@ void handleRoot() {
   configServer.send(200, "text/html", s);
 }
 
-void HandleNotFound(){
-    configServer.send(403, "text/html", "Page Not Found");
-}
-
-void SetupHttpHandlers()
-{
+void SetupHttpHandlers() {
   // -- Set up required URL handlers on the web server.
   configServer.on("/", [] { handleRoot(); });
   configServer.on("/status", [] { handleStatus(); });
-
 #ifdef EBUS_INTERNAL
   configServer.on("/commands/list", [] { handleCommandsList(); });
   configServer.on("/commands/download", [] { handleCommandsDownload(); });
@@ -174,5 +165,6 @@ void SetupHttpHandlers()
   configServer.on("/values", [] { handleValues(); });
 #endif
   configServer.on("/restart", [] { restart(); });
+  configServer.on("/config", [] { iotWebConf.handleConfig(); });
   configServer.onNotFound([]() { iotWebConf.handleNotFound(); });
 }
