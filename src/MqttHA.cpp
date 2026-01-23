9c2fff3284eae364f0c5caaa1f2d08d82b1bca58
Roland Jax <roland.jax@liwest.at>
2026-01-23 15:53:43 +0100

    uptime min -> s

--- src/MqttHA.cpp
+++ src/MqttHA.cpp
@@ -312,8 +312,8 @@ MqttHA::Component MqttHA::createDiagnostic(const std::string& component,
 MqttHA::Component MqttHA::createDiagnosticUptime() const {
   Component c = createDiagnostic("sensor", "uptime", "Uptime");
   c.fields["state_topic"] = createStateTopic("state", "uptime");
-  c.fields["unit_of_measurement"] = "min";
-  c.fields["value_template"] = "{{((value|float)/1000/60)|round(1)}}";
+  c.fields["unit_of_measurement"] = "s";
+  c.fields["value_template"] = "{{((value|float)/1000)|int}}";
   c.fields["icon"] = "mdi:clock-outline";
 
   c.device["name"] = thingName;
