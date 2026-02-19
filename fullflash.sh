a0cc52d8391f4787b1861ea07e2d2079ead28036
Daniel Kucera <daniel.kucera@gmail.com>
2026-02-19 22:24:50 +0100

    Ci updates (#276)
    * ci updates
    
    * fullflash param

--- fullflash.sh
+++ fullflash.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-OF=firmware-fullflash-HW_v5.x.bin 
+set -eu
 
-tr '\0' '\377' < /dev/zero | dd bs=1 count=$((0x10000)) of=$OF
-dd if=.pio/build/esp32-c3/bootloader.bin of=$OF conv=notrunc
-dd if=.pio/build/esp32-c3/partitions.bin of=$OF bs=1 seek=$((0x8000)) conv=notrunc
-dd if=$HOME/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin of=$OF bs=1 seek=$((0xe000)) conv=notrunc
-dd if=.pio/build/esp32-c3/firmware.bin of=$OF bs=1 seek=$((0x10000))
+ENV_NAME="${1:-esp32-c3}"
+OF="${2:-firmware-fullflash-HW_v5.x.bin}"
 
+tr '\0' '\377' < /dev/zero | dd bs=1 count=$((0x10000)) of="$OF"
+dd if=".pio/build/${ENV_NAME}/bootloader.bin" of="$OF" conv=notrunc
+dd if=".pio/build/${ENV_NAME}/partitions.bin" of="$OF" bs=1 seek=$((0x8000)) conv=notrunc
+dd if="$HOME/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin" of="$OF" bs=1 seek=$((0xe000)) conv=notrunc
+dd if=".pio/build/${ENV_NAME}/firmware.bin" of="$OF" bs=1 seek=$((0x10000))
