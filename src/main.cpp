d6b34d58fb69cdad067bdd7bf925fadf144227fb
Daniel Kucera <daniel.kucera@gmail.com>
2026-03-02 07:23:05 +0100

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,19 @@
 #endif
 
 #include <ESPmDNS.h>
-#include <IotWebConfESP32HTTPUpdateServer.h>
 #include <esp_task_wdt.h>
 
+#include "UpgradeManager.hpp"
 #include "esp32c3/rom/rtc.h"
 #include "esp_sntp.h"
 #include "http.hpp"
-
-HTTPUpdateServer httpUpdater;
 
 #if !defined(EBUS_INTERNAL)
 TaskHandle_t Task1;
 #endif
 
 Preferences preferences;
+UpgradeManager upgradeManager;
 
 // minimum time of reset pin
 #define RESET_MS 1000
@@ -877,14 +876,19 @@ void setup() {
   }
 
   SetupHttpHandlers();
-
-  iotWebConf.setupUpdateServer(
-      [](const char* updatePath) {
-        httpUpdater.setup(&configServer, updatePath);
-      },
-      [](const char* userName, char* password) {
-        httpUpdater.updateCredentials(userName, password);
-      });
+  upgradeManager.begin(&configServer);
+  upgradeManager.setPreUpgradeHook([]() {
+#if defined(EBUS_INTERNAL)
+    ebus::serviceRunner->stop();
+    schedule.stop();
+    clientManager.stop();
+#else
+    if (Task1 != nullptr) {
+      vTaskDelete(Task1);
+      Task1 = nullptr;
+    }
+#endif
+  });
 
   set_pwm(atoi(pwm_value));
 
@@ -967,7 +971,12 @@ void setup() {
   mqttha.publishComponents();
 #else
   xTaskCreate(data_loop, "data_loop", 10000, NULL, 1, &Task1);
-  ArduinoOTA.onStart([]() { vTaskDelete(Task1); });
+  ArduinoOTA.onStart([]() {
+    if (Task1 != nullptr) {
+      vTaskDelete(Task1);
+      Task1 = nullptr;
+    }
+  });
 #endif
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     printf("Progress: %u%%\r", (progress / (total / 100)));
