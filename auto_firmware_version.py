bcbd7ccdfdf48f5be8e992c5ef772b8611ae9845
Daniel Kucera <daniel.kucera@gmail.com>
2026-03-02 09:50:12 +0100

    added force version for faster local builds

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -1,10 +1,17 @@
 import subprocess
+from pathlib import Path
 
 Import("env")
 
 def get_firmware_specifier_build_flag():
-    ret = subprocess.run(["git", "describe", "--tags", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses any tags
-    build_version = ret.stdout.strip()
+    force_version_file = Path("force_version.txt")
+    if force_version_file.exists():
+        build_version = force_version_file.read_text(encoding="utf-8").strip()
+        if not build_version:
+            raise ValueError("force_version.txt exists but is empty")
+    else:
+        ret = subprocess.run(["git", "describe", "--tags", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses any tags
+        build_version = ret.stdout.strip()
     build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
     print ("Firmware Revision: " + build_version)
     return (build_flag)
