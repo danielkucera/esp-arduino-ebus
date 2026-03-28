91966cf27d826a354a644ff57973ff6764d0832d
Daniel Kucera <daniel.kucera@gmail.com>
2026-03-28 16:50:34 +0100

    added cron files

---
+++ include/Cron.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#if defined(EBUS_INTERNAL)
+
+#include <cJSON.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+class Cron {
+ public:
+  bool initFileSystem();
+
+  void start();
+  void stop();
+
+  void insertRule(const cJSON* doc);
+  void removeRule(const std::string& id);
+
+  int64_t loadRules();
+  int64_t saveRules() const;
+  static int64_t wipeRules();
+
+  const std::string getRulesJson() const;
+
+  static const std::string evaluate(const cJSON* doc);
+
+ private:
+  struct Rule {
+    std::string id;
+    std::string schedule;
+    std::string commandKey;
+    std::string valueJson;
+    bool enabled = true;
+    int64_t lastTriggeredMinute = -1;
+  };
+
+  std::unordered_map<std::string, Rule> rules;
+
+  volatile bool stopRunner = false;
+  TaskHandle_t taskHandle = nullptr;
+
+  mutable portMUX_TYPE rulesMux = portMUX_INITIALIZER_UNLOCKED;
+
+  static void taskFunc(void* arg);
+  void tick();
+};
+
+extern Cron cron;
+
+#endif
