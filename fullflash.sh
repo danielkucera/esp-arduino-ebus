6cb9186c8e98a00ef28564d02fd101b0e37edee4
Roland Jax <roland.jax@liwest.at>
2026-09-05 23:50:00 +0200

    second try

--- fullflash.sh
+++ fullflash.sh
@@ -4,10 +4,9 @@ set -eu
 
 ENV_NAME="${1:-esp32-c3}"
 OF="${2:-firmware-fullflash-HW_v5.x.bin}"
-BASE_ENV="${3:-$ENV_NAME}"
 
 tr '\0' '\377' < /dev/zero | dd bs=1 count=$((0x10000)) of="$OF"
-dd if=".pio/build/${BASE_ENV}/bootloader.bin" of="$OF" conv=notrunc
-dd if=".pio/build/${BASE_ENV}/partitions.bin" of="$OF" bs=1 seek=$((0x8000)) conv=notrunc
-dd if=".pio/build/${BASE_ENV}/ota_data_initial.bin" of="$OF" bs=1 seek=$((0xe000)) conv=notrunc
+dd if=".pio/build/${ENV_NAME}/bootloader.bin" of="$OF" conv=notrunc
+dd if=".pio/build/${ENV_NAME}/partitions.bin" of="$OF" bs=1 seek=$((0x8000)) conv=notrunc
+dd if=".pio/build/${ENV_NAME}/ota_data_initial.bin" of="$OF" bs=1 seek=$((0xe000)) conv=notrunc
 dd if=".pio/build/${ENV_NAME}/firmware.bin" of="$OF" bs=1 seek=$((0x10000))
